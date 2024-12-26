a2eb9216a2ea32a47dc5a05457f6647809d17151
dorukbatur <dbatur14@gmail.com>
2024-12-26 13:48:14 +0300

    changing for demo purposes

--- app/src/main/java/com/example/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/example/kotlindemo/MainActivity.kt
@@ -14,34 +14,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlindemo.ui.theme.KotlinDemoTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            KotlinDemoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    KotlinDemoTheme {
-        Greeting("Andr id")
+        setContentView(R.layout.activity_main)
     }
 }
